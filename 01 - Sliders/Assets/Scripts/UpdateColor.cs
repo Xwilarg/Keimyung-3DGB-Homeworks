6465ce0dd66de36c258e2bb17e669bd519259965
Xwilarg <xwilarg@yahoo.fr>
2019-09-23 11:38:08 +0900

    Add firsts 2 projects

---
+++ 01 - Sliders/Assets/Scripts/UpdateColor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class UpdateColor : MonoBehaviour
+{
+    public Slider red, green, blue;
+    public AudioSource source;
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void UpdateImageColor()
+    {
+        image.color = new Color(red.value, green.value, blue.value);
+        if (red.value == 1f && green.value == 1f && blue.value == 1f)
+            source.enabled = true;
+    }
+}
---
+++ 02 - Pickables/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Prefab for star UI")]
+    private GameObject starUI;
+    [SerializeField]
+    [Tooltip("Parent UI for where star UI are placed")]
+    private Transform parentUI;
+    [SerializeField]
+    private Sprite starUIEmpty, starUIFull;
+    [SerializeField]
+    private GameObject victoryUI;
+
+    private SpriteRenderer sr;
+    private Rigidbody2D rb;
+
+    private const float speed = 300f; // Player speed
+    private const float jumpDistance = 0.65f; // Max distance with the floor so the player can jump
+    private const float jumpForce = 15f; // Force of the player jump
+    private const int starCount = 5; // Max amount of star the player can get
+
+    private int currStar; // Current amount of star the player have
+    private Image[] allStarUI; // All Image of the UI of the star
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        allStarUI = new Image[starCount];
+        for (int i = 0; i < starCount; i++)
+        {
+            GameObject ui = Instantiate(starUI, parentUI);
+            ui.transform.Translate(new Vector2(i * 100f, 0f));
+            allStarUI[i] = ui.GetComponent<Image>();
+        }
+        currStar = 0;
+    }
+
+    private void Update()
+    {
+        // Movements
+        float horizontal = Input.GetAxis("Horizontal");
+        if (horizontal < 0f)
+            sr.flipX = true;
+        else if (horizontal > 0f)
+            sr.flipX = false;
+
+        // Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            int layer = 1 << 8; // "Floor" layer
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, jumpDistance, layer);
+            if (hit.distance > 0f)
+            {
+                rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+
+        // So the player don't return himself
+        if (Mathf.Abs(transform.rotation.eulerAngles.z) > 80f)
+        {
+            transform.rotation = Quaternion.identity;
+        }
+
+        rb.velocity = new Vector2(horizontal * Time.deltaTime * speed, rb.velocity.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Star"))
+        {
+            allStarUI[currStar].sprite = starUIFull;
+            currStar++;
+            if (currStar == starCount)
+                victoryUI.SetActive(true);
+            Destroy(collision.gameObject);
+        }
+    }
+}
